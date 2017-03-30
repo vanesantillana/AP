11e6dec5c4c30312da665fe37049a6834faa16b3
vanesantillana <vanessa.santillana@ucsp.edu.pe>
2017-03-30 15:50:51 -0500

    pingpong

--- MPI/nuevo.c
+++ MPI/nuevo.c
@@ -1,30 +1,35 @@
 #include <stdio.h>
-#include <string.h> 
-#include <mpi.h> 
+#include <string.h>
+#include <mpi.h>
 
-const int  MAX_STRING = 100;
+const int MAX_STRING = 100;
 
-int main(void) {
-	char greeting[MAX_STRING];
-	int comm_sz; 
-	int my_rank; 
+int main(void)
+{
+    char num[MAX_STRING];
+    int num_proceso;
+    int id_proceso;
+	
+    MPI_Init(NULL,NULL);
+    MPI_Comm_size(MPI_COMM_WORLD , &num_procesos);
+    MPI_Comm_rank(MPI_COMM_WORLD , &id_procesos );
 
-	MPI_Init(NULL, NULL);
-	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    if(id_procesos != 0)
+    {
+        sprintf(num," Salgo desde el proceso %d al %d!",id_procesos,num_procesos);
+        MPI_Send(num,strlen(num)+1, MPI_CHAR , 0, 0, MPI_COMM_WORLD);        
+    }
+    else
+    {
+        printf("Salgo dede el proceso %d al %d!\n",id_procesos,num_procesos);
+        for(int i=1; i< num_procesos;i++)
+        {
+            MPI_Recv(num,MAX_STRING,MPI_CHAR,i,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+            printf("%s\n",num);
+        }
+    }
+    
+    MPI_Finalize();
+    return 0;
 
- 	if (my_rank != 0) {
- 		sprintf(greeting, "Greetings from process %d of %d!",my_rank, comm_sz);
- 		MPI_Send(greeting, strlen(greeting)+1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
- 	} 
- 	else {
- 		printf("Greetings from process %d of %d!\n", my_rank,comm_sz);
- 		for (int q = 1; q < comm_sz; q++) {
- 			MPI_Recv(greeting, MAX_STRING, MPI_CHAR, q,0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
- 		printf("%s\n", greeting);
- 		}
- 	}
-
- MPI_Finalize();
- return 0;
-} 
+}
